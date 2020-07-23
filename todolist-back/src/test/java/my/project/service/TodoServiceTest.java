f57d146f66617b9fbcb1584a8b5b3a7dd5bccf22
SharyginD <dima.sharygin.1999@mail.ru>
2020-07-23 21:51:39 +0300

    working TodoList

--- todolist-back/src/test/java/my/project/service/TodoServiceTest.java
+++ todolist-back/src/test/java/my/project/service/TodoServiceTest.java
@@ -1,22 +1,11 @@
 package my.project.service;
 
-import my.project.domain.dto.Todo;
-import my.project.domain.entity.TodoEntity;
-import my.project.domain.entity.UserEntity;
-import my.project.exception.customException.ResourceNotFoundException;
 import my.project.repository.TodoRepository;
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,42 +16,4 @@ public class TodoServiceTest {
 
     @MockBean
     private TodoRepository repository;
-
-//    private List<Todo> listDTO = Arrays.asList(
-//            new Todo(1, "Note 1"),
-//            new Todo(2, "Note 2"),
-//            new Todo(3, "Note 3"));
-//
-//    private List<TodoEntity> listEntity = Arrays.asList(
-//            new TodoEntity(1, "Note 1", new UserEntity()),
-//            new TodoEntity(2, "Note 2", new UserEntity()),
-//            new TodoEntity(3, "Note 3", new UserEntity()));
-
-//    @Test
-//    public void getAllTest() {
-//        Mockito.when(repository.findAll()).thenReturn(listEntity);
-//        Assert.assertEquals(service.getAll(), listDTO);
-//    }
-//
-//    @Test
-//    public void saveTest() {
-//        Todo todo = new Todo(1, "Hello");
-//        TodoEntity entity = new TodoEntity(1, "Hello", new UserEntity());
-//        Mockito.when(repository.save(entity)).thenReturn(entity);
-//        Assert.assertEquals(service.save(todo), todo);
-//    }
-//
-//    @Test
-//    public void successDeleteTest() {
-//        TodoEntity entity = new TodoEntity(1, "Hello", new UserEntity());
-//        Todo todo = new Todo(1, "Hello");
-//        Mockito.when(repository.findById(1)).thenReturn(Optional.of(entity));
-//        Assert.assertEquals(service.delete(1), todo);
-//    }
-//
-//    @Test(expected = ResourceNotFoundException.class)
-//    public void failedDeleteTest() {
-//        Mockito.when(repository.findById(1)).thenThrow(new ResourceNotFoundException("error"));
-//        service.delete(1);
-//    }
 }
--- todolist-back/src/test/java/my/project/service/UserServiceTest.java
+++ todolist-back/src/test/java/my/project/service/UserServiceTest.java
@@ -1,24 +1,11 @@
 package my.project.service;
 
-import my.project.domain.Role;
-import my.project.domain.dto.Todo;
-import my.project.domain.dto.User;
-import my.project.domain.entity.TodoEntity;
-import my.project.domain.entity.UserEntity;
-import my.project.exception.customException.NonUniqueUserException;
 import my.project.repository.UserRepository;
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,34 +16,4 @@ public class UserServiceTest {
 
     @MockBean
     private UserRepository repository;
-
-//    private List<User> listUserDTO = Arrays.asList(
-//            new User(1, "Login 1", "Password 1", Role.ROLE_USER, new ArrayList<Todo>()),
-//            new User(2, "Login 2", "Password 2", Role.ROLE_USER, new ArrayList<Todo>()));
-//
-//    private List<UserEntity> listUserEntity = Arrays.asList(
-//            new UserEntity(1, "Login 1", "Password 1", Role.ROLE_USER, new ArrayList<TodoEntity>()),
-//            new UserEntity(2, "Login 2", "Password 2", Role.ROLE_USER, new ArrayList<TodoEntity>()));
-//
-//    @Test
-//    public void getAllTest() {
-//        Mockito.when(repository.findAll()).thenReturn(listUserEntity);
-//        Assert.assertEquals(service.getAll(), listUserDTO);
-//    }
-//
-//    @Test
-//    public void SuccessSaveTest() {
-//        User user = new User(1, "Login 1", "Password 1", Role.ROLE_USER, new ArrayList<Todo>());
-//        UserEntity entity = new UserEntity(1, "Login 1", "Password 1", Role.ROLE_USER, new ArrayList<TodoEntity>());
-//        Mockito.when(repository.existsByLogin(user.getLogin())).thenReturn(false);
-//        Mockito.when(repository.save(entity)).thenReturn(entity);
-//        Assert.assertEquals(service.save(user), user);
-//    }
-//
-//    @Test(expected = NonUniqueUserException.class)
-//    public void failedSaveTest() {
-//        User user = new User(1, "Login 1", "Password 1", Role.ROLE_USER, new ArrayList<Todo>());
-//        UserEntity entity = new UserEntity(1, "Login 1", "Password 1", Role.ROLE_USER, new ArrayList<TodoEntity>());
-//        Mockito.when(repository.existsByLogin(user.getLogin())).thenReturn(true);
-//    }
 }
